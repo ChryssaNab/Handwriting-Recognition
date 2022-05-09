52dfce524ac5e1b71fe269fc5132fdd098f095de
Luca Mueller <d.l.mueller@student.rug.nl>
2022-05-09 19:59:04 +0200

    added preprocessing functions

--- src/task3/data.py
+++ src/task3/data.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 import tensorflow as tf
+from typing import Tuple
+from sklearn.model_selection import train_test_split
+
 
 # your path to the IAM folder
-local_path_to_iam = "C:\\Users\\Luca\Desktop\\HWR"
+local_path_to_iam = "C:\\Users\\muell\\Desktop\\HWR\\Task 3\\Data"
 data_dir = Path(local_path_to_iam) / "IAM-data"
 img_dir = data_dir / "img"
 
@@ -54,7 +57,7 @@ def load_dataset(data_dict: dict) -> tf.data.Dataset:
     # load images from paths
     images = images.map(lambda x: tf.io.read_file(x))
     images = images.map(lambda x: tf.io.decode_png(x))
-    images = images.map(lambda x: tf.squeeze(x))
+    #images = images.map(lambda x: tf.squeeze(x))
 
     # combine images, filenames and labels
     dataset = tf.data.Dataset.zip((images, files, labels))
@@ -62,8 +65,49 @@ def load_dataset(data_dict: dict) -> tf.data.Dataset:
     return dataset
 
 
+def distortion_free_resize(image: tf.Tensor, img_size: Tuple[int, int]) -> tf.Tensor:
+    w, h = img_size
+    image = tf.image.resize(image, size=(h, w), preserve_aspect_ratio=True)
+
+    # Check the amount of padding needed to be done.
+    pad_height = h - tf.shape(image)[0]
+    pad_width = w - tf.shape(image)[1]
+
+    # Only necessary if you want to do same amount of padding on both sides.
+    if pad_height % 2 != 0:
+        height = pad_height // 2
+        pad_height_top = height + 1
+        pad_height_bottom = height
+    else:
+        pad_height_top = pad_height_bottom = pad_height // 2
+
+    if pad_width % 2 != 0:
+        width = pad_width // 2
+        pad_width_left = width + 1
+        pad_width_right = width
+    else:
+        pad_width_left = pad_width_right = pad_width // 2
+
+    image = tf.pad(
+        image,
+        paddings=[
+            [pad_height_top, pad_height_bottom],
+            [pad_width_left, pad_width_right],
+            [0, 0],
+        ],
+    )
+
+    image = tf.transpose(image, perm=[1, 0, 2])
+    image = tf.image.flip_left_right(image)
+    return image
+
+
 def scale_img(image: tf.Tensor) -> tf.Tensor:
-    return tf.cast(image, tf.float32) / 255.
+    return tf.cast(image, tf.float32) / 255.1
+
+
+def preprocess(image: tf.Tensor) -> tf.Tensor:
+    return tf.keras.applications.mobilenet.preprocess_input(tf.cast(image, tf.float32))
 
 
 def remove_filenames(dataset: tf.data.Dataset) -> tf.data.Dataset:
@@ -72,20 +116,47 @@ def remove_filenames(dataset: tf.data.Dataset) -> tf.data.Dataset:
     return tf.data.Dataset.zip((x, y))
 
 
-def test():
+def split_data(i, l):
+    x_tr, x_te, y_tr, y_te = train_test_split(i, l, train_size=0.95)
+    return x_tr, x_te, y_tr, y_te
+
+
+def test(image_width: int, image_height: int):
     data_dict = load_data_dict()
+    # TODO: what does this line mean?
+    #im, la, \
     dataset = load_dataset(data_dict)
 
-    #preprocessing example
-    #dataset = dataset.map(lambda x, f, y: (scale_img(x), f, y))
-    #dataset = dataset.apply(remove_filenames)
+    # preprocessing example
+    # preprocessing using inbuilt function
+    dataset = dataset.map(lambda x, f, y: (preprocess(x), f, y))
+    # padding
+    dataset = dataset.map(lambda x, f, y: (distortion_free_resize(x, img_size=(image_width, image_height)), f, y))
+    dataset = dataset.apply(remove_filenames)
 
     it = (dataset.as_numpy_iterator())
     for i, e in enumerate(it):
-        print(e)
+        #print(e)
+        pass
         if i > 3:
             break
 
+    return dataset
+
 
 if __name__ == "__main__":
-    test()
+
+    image_width = 512
+    image_height = 64
+    data = test(image_width, image_height)
+    images, labels = zip(*data)
+    x_train, x_test, y_train, y_test = split_data(images, labels)
+
+    print(len(x_train))
+    print(len(y_train))
+    print(len(x_test))
+    print(len(y_test))
+
+    import matplotlib.pyplot as plt
+    plt.imshow(x_train[0])
+    plt.show()
