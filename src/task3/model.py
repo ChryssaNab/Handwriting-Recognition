9ecc1e5681eba08c944f4738e91c34f86c7a8c91
Luca Mueller <d.luca.mueller@gmail.com>
2022-05-22 13:33:50 +0200

    added width parameter

--- src/task3/model.py
+++ src/task3/model.py
@@ -23,17 +23,24 @@ class CTCLayer(tf.keras.layers.Layer):
         return y_pred
 
 
-def build_LSTM_model(n_classes: int) -> tf.keras.Model:
+def build_LSTM_model(n_classes: int, width: int = 800) -> tf.keras.Model:
     """
     Model architecture adapted from:
     Handwritten Text Recognition in Historical Documents, p.38
     https://web.archive.org/web/20210814184909id_/https://repositum.tuwien.at/bitstream/20.500.12708/5409/2/Scheidl%20Harald%20-%202018%20-%20Handwritten%20text%20recognition%20in%20historical%20documents.pdf
 
     :param n_classes: number of classes to predict (i.e. number of characters), n_classes < 100
+    :param width: width of input image
     :return: the model as keras Sequential model
     """
 
-    input_img = tf.keras.Input(shape=(800, 64, 1), name="Image")
+    height, channels = 64, 1
+    logit_length = width // 4
+
+    if width % logit_length:
+        raise ValueError("input width not divisible by 4")
+
+    input_img = tf.keras.Input(shape=(width, height, channels), name="Image")
     input_label = tf.keras.layers.Input(name="Label", shape=(None,))
 
     conv = tf.keras.layers.Conv2D(64, 5, padding="same", activation="relu", name="Conv_1")(input_img)
@@ -51,7 +58,7 @@ def build_LSTM_model(n_classes: int) -> tf.keras.Model:
     conv = tf.keras.layers.BatchNormalization(name="BatchNorm_2")(conv)
     conv = tf.keras.layers.Conv2D(512, 3, padding="same", activation="relu", name="Conv_7")(conv)
     conv = tf.keras.layers.MaxPool2D(pool_size=(1, 2), padding="same", name="MaxPool_6")(conv)
-    flat = tf.keras.layers.Reshape((200, 512), name="Collapse")(conv)
+    flat = tf.keras.layers.Reshape((logit_length, 512), name="Collapse")(conv)
     lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512, activation='tanh', return_sequences=True),
                                          name="BiDir_LSTM")(flat)
     lstm = tf.keras.layers.Dense(n_classes, activation=None, name="Output_Dense")(lstm)
