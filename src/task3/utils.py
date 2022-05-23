99838de36838b9eaf3ee9f53a05a25586a6dd1b1
Luca Mueller <d.l.mueller@student.rug.nl>
2022-05-23 20:08:52 +0200

    added TODO

--- src/task3/utils.py
+++ src/task3/utils.py
@@ -10,3 +10,5 @@ def show_sample(X: tf.Tensor, y: tf.Tensor):
     plt.imshow(tf.transpose(tf.image.flip_left_right(X), [1, 0, 2]), cmap='Greys')
     plt.title(y)
     plt.show()
+
+# TODO: model checkpoints, tensorboard, parameter search, crossvalidation
