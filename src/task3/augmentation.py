1b780ed6f6bcb587c804361d6b25f5b7f13b2e0c
Shray15 <54475344+Shray15@users.noreply.github.com>
2022-05-26 23:22:32 +0200

    Update augmentation.py

--- src/task3/augmentation.py
+++ src/task3/augmentation.py
@@ -2,4 +2,15 @@
 Data augmentation tools for IAM
 """
 
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def data_augment():
+    data_augmentation = tf.keras.Sequential([
+        # layers.RandomBrightness(-.5,0.5),
+        layers.RandomRotation(0.2),
+        layers.RandomContrast(0.5)
+    ])
+    return data_augmentation
+
 # TODO: add data augmentation tools
