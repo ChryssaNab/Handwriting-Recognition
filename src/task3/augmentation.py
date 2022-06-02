8aa5db86ae8221ba07420757877b1b9854ee1b41
Luca Mueller <d.luca.mueller@gmail.com>
2022-06-02 13:50:44 +0200

    updated settings

--- src/task3/augmentation.py
+++ src/task3/augmentation.py
@@ -11,11 +11,10 @@ def data_augment():
 
     :return: augmentation layers as Sequential model
     """
-
     data_augmentation = Sequential([
-        layers.RandomTranslation(0.1, 0.1, fill_mode="constant", fill_value=0.0),
-        layers.RandomRotation(0.1),
-        layers.RandomContrast(0.5),
-        layers.GaussianNoise(0.1),
+        layers.RandomRotation(0.02),
+        layers.RandomTranslation(0.05, 0.1, fill_mode="constant", fill_value=0.0),
+        layers.RandomContrast(0.9),
+        layers.GaussianNoise(0.05),
     ], name="Data_Augmentation")
     return data_augmentation
